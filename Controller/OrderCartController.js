6b6c04715765a26e61c5ecd71d3d93dd164cb76e
Shafi Miah <32789015+ShafiMiah@users.noreply.github.com>
2021-02-14 20:53:58 +0100

    Delete Controller directory

--- Controller/OrderCartController.js
+++
@@ -1,6 +0,0 @@
-﻿(function ( flyingDutchman,underForeignFlag, $, document) {
-//
-    $(function () {
-
-    });
-}(window.flyingDutchman = window.flyingDutchman || {}, window.underForeignFlag = window.underForeignFlag || {}, window.jQuery, document));
